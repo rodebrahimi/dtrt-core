60d9c86ffccecf950aaf8d5d16a40b460fd80dc7
Rod Ebrahimi <rodebrahimi@gmail.com>
2023-06-17 19:56:02 -0400

    small updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,11 +26,11 @@ module.exports = {
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
+        //gtagConfig: {
+        //  optimize_id: "OPT_CONTAINER_ID",
+        //  anonymize_ip: true,
+        //  cookie_expires: 0,
+        //},
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
@@ -46,6 +46,7 @@ module.exports = {
         },
       },
     },
+    'gatsby-plugin-robots-txt',
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,

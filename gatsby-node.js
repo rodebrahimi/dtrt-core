b83cb2c44a1e0830c69fed5239ef80fe79eadde5
Rod Ebrahimi <rodebrahimi@gmail.com>
2023-06-22 10:34:24 -0400

    remove unnecessary pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,10 +9,11 @@
  */
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
-  createPage({
+/**  createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
   })
+  **/
 }
